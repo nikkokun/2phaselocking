44a28761b450ee58ef375c4a92ef19b18606ac61
nikkokun <mizutaninikkou@gmail.com>
2019-05-14 20:40:06 +0900

    First Commit

---
+++ Release/CMakeFiles/2phaselocking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2phaselocking.dir/main.cpp.o"
+  "2phaselocking.pdb"
+  "2phaselocking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2phaselocking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
